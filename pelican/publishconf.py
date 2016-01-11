67593e45cb74962d086d233d2f3ec4a08aa57e47
40323250 <b850617@yahoo.com.tw>
2016-01-11 09:50:28 +0800

    a

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -27,8 +27,8 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
          ('Python', 'http://python.org/'),
-         ('班級個人作業', 'http://2015fallhw.github.io/2015fallcadpb/user/40323235/'),
-         ('You can modify those links in your config file', '#'),)
+         ('40323235 個人網頁', 'http://2015fallhw.github.io/2015fallcadpb/user/40323235/'),
+         
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
